d5dab9174a71647d5ac29e029aea42a240afacd9
Fede <federico.andres.perez@fing.edu.uy>
2020-04-01 13:06:17 -0300

    Mostrando un mensaje de forma condicional

--- src/App.js
+++ src/App.js
@@ -22,6 +22,9 @@ function App() {
     guardarCitas(nuevasCitas);
   }
 
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas'; 
+
   return (
     <Fragment>
     <h1>Veterinaria</h1>
@@ -33,7 +36,7 @@ function App() {
           />
         </div>
         <div className="one-half column">
-          <h2>Administra tus citas</h2>
+          <h2>{titulo}</h2>
           {citas.map(cita => (
             <Cita
               key={cita.id}
